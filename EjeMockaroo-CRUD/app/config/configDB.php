847e5ec0bdfc3082adaef676066e4d7448e3375b
gonzaloandres12 <gonzaloandres93@gmail.com>
2023-01-31 18:23:14 +0100

    a

--- EjeMockaroo-CRUD/app/config/configDB.php
+++ EjeMockaroo-CRUD/app/config/configDB.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER','localhost');
 define('DB_USER','root');
-define('DB_PASSWD','7388');
+define('DB_PASSWD','root');
 define('DATABASE_CLIENTES','TestClientes');
 define('DATABASE_LOGIN','LoginDB');
 
